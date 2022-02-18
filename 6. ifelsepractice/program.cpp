8021acdd4e57b2bda0dda46b43ff279abb7297f0
purva bhokare <purvab08@gmail.com>
2022-02-18 19:03:06 +0530

    if-else practice

---
+++ 6. ifelsepractice/program.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main (void)
+{
+    int age;
+    printf ("enter your age\n");
+    scanf ("%d", &age);
+
+    if (age>=18){
+        printf ("you can vote!");
+
+    }
+    else {
+        printf ("you cannot vote!");
+    }
+
+}
